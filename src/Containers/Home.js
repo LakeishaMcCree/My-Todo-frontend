ff6ad47e19c742cd297a961ffc772322f765b000
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-01-30 01:31:02 -0500

    set up App and index and added all react and redux tools to package.json

---
+++ src/Containers/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react' 
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My Todo App</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
